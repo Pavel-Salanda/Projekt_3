08722c207e0ba0ae660dae59138add3e48f558dd
Pavel Šalanda <pavel.salanda@gmail.com>
2023-11-12 09:16:49 +0100

    The second version. Correction of the "party results" function (correct immersion of the for cycle). Remove parentheses from return. Removing return from create csv function.

--- data_volby_2017.py
+++ data_volby_2017.py
@@ -32,17 +32,17 @@ def server_response_processing (url):
     else:
         print('The request failed with code:', response.status_code)
         exit()
-    return (html)
+    return html
 
 def create_soup_object (html):
     '''Creates a BeautifulSoup object'''
     soup = BeautifulSoup(html, 'html.parser')
-    return (soup)
+    return soup
 
 def find_table (soup):
     '''Finds all tables on the page'''
     tables = soup.find_all('table')
-    return (tables)
+    return tables
 
 def data_from_tables (tables):
     '''Creates an empty list for output.
@@ -57,7 +57,7 @@ def data_from_tables (tables):
                 text_line.pop()
                 if '-' not in text_line:
                     data.append(text_line)
-    return(data)
+    return data
 
 def find_links (tables):
     '''Creates an empty list for links.
@@ -69,7 +69,7 @@ def find_links (tables):
             link_and_number = link.get('href'),link.get_text('hrev')
             if 'X' != link.get_text('hrev'):
                 links_to_data.append(link_and_number)
-    return (links_to_data)
+    return links_to_data
 
 def link_verification (url):
     '''It will verify the links to individual village results'''
@@ -87,7 +87,7 @@ def link_verification (url):
            data.append(a)
         html.append(data)
     new_html = [html[i:i + 2] for i in range(0, len(html), 2)]
-    return (new_html)
+    return new_html
 
 def create_soup (html):
     '''Creates a soup object for individual village links'''
@@ -99,7 +99,7 @@ def create_soup (html):
         soup = create_soup_object(string)
         data.append(soup)
         soup_links.append(data)
-    return (soup_links)
+    return soup_links
 
 def summary_data_village (links):
     '''Gets summary data for a village'''
@@ -115,7 +115,7 @@ def summary_data_village (links):
                 data_village.append(voter_table.find_all('td')[4].get_text().replace('\xa0', '')) #issued envelopes
                 data_village.append(voter_table.find_all('td')[7].get_text().replace('\xa0', '')) #valid votes
         data.append(data_village)
-    return (data)
+    return data
 
 def party_results (links):
     '''Find the table with the number of votes for the candidate parties'''
@@ -129,12 +129,12 @@ def party_results (links):
                 table_party = a.find_all('table')[1:]
                 for table in table_party:
                     lines = table.find_all('tr')[2:]
-                for line in lines:
-                    columns = line.find_all('td')[1:3]
-                    text_line = [column.get_text() for column in columns]
-                    data.append(text_line)
+                    for line in lines:
+                        columns = line.find_all('td')[1:3]
+                        text_line = [column.get_text() for column in columns]
+                        data.append(text_line)
         data_party.append(data)
-    return (data_party)
+    return data_party
 
 def create_dictionary (village,data,party):
     '''Creates a dictionary where all data is combined'''
@@ -157,7 +157,7 @@ def create_dictionary (village,data,party):
                         dictionary_village[e[0]] = e[1]
         dictionary_data.append(dictionary_village)
     dictionary_data.append(dictionary_village)
-    return (dictionary_data)
+    return dictionary_data
 
 def create_csv (dictionary, name):
     '''Creates a csv file'''
@@ -166,7 +166,7 @@ def create_csv (dictionary, name):
         writer = csv.DictWriter(file, fieldnames=dictionary[0].keys(), delimiter=';')
         writer.writeheader()
         writer.writerows(dictionary)
-    return (print(f'CSV file "{csv_file}" has been created.'))
+    print(f'CSV file "{csv_file}" has been created.')
 
 
 def main(url):
@@ -180,6 +180,7 @@ def main(url):
     party = party_results(link_soup)
     final_data = create_dictionary(data,table_of_voters,party)
     creation_of_csv = create_csv(final_data, file_name)
+
 
 if __name__ == '__main__':
     url = sys.argv[1]
